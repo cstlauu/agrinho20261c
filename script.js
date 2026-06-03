bc4a4b37f9b0046e8ac9ec1b2f213a63552bb0b0
cstlauu <lauhanny.costa@escola.pr.gov.br>
2026-06-03 16:54:32 -0300

    .

--- script.js
+++ script.js
@@ -1,17 +1,100 @@
-const contador = document.getElementById("contador");
+document.getElementById("saibaMais")
+.addEventListener("click", () => {
 
-const valorFinal = 5000;
-let valorAtual = 0;
+    document
+    .getElementById("historia")
+    .scrollIntoView({
+        behavior:"smooth"
+    });
 
-const intervalo = setInterval(() => {
+});
 
-    valorAtual += 50;
+let progresso = 0;
 
-    contador.textContent = valorAtual.toLocaleString("pt-BR");
+function aumentar(){
 
-    if (valorAtual >= valorFinal) {
-        contador.textContent = valorFinal.toLocaleString("pt-BR");
-        clearInterval(intervalo);
+    if(progresso < 100){
+
+        progresso += 10;
+
+        document
+        .getElementById("progresso")
+        .style.width = progresso + "%";
+
+        document
+        .getElementById("valor")
+        .innerHTML = progresso + "%";
+
+    }
+
+}
+
+const curiosidades = [
+
+"🌱 Uma árvore pode absorver até 22 kg de CO₂ por ano.",
+
+"🚜 Agricultura de precisão reduz desperdícios de insumos.",
+
+"💧 Sistemas modernos economizam até 50% de água.",
+
+"🌾 Rotação de culturas melhora a fertilidade do solo.",
+
+"🐝 Polinizadores são essenciais para grande parte da produção agrícola."
+
+];
+
+function novaCuriosidade(){
+
+    let sorteio =
+    Math.floor(
+        Math.random() *
+        curiosidades.length
+    );
+
+    document
+    .getElementById("textoCuriosidade")
+    .innerHTML =
+    curiosidades[sorteio];
+
+}
+
+function responder(correto){
+
+    let resultado =
+    document.getElementById("resultadoQuiz");
+
+    if(correto){
+
+        resultado.innerHTML =
+        "✅ Correto! A rotação de culturas preserva o solo.";
+
+        resultado.style.color = "green";
+
+    }else{
+
+        resultado.innerHTML =
+        "❌ Resposta incorreta.";
+
+        resultado.style.color = "red";
+
+    }
+
+}
+
+let numero = 0;
+
+const contador = setInterval(() => {
+
+    numero += 50;
+
+    document
+    .getElementById("contador")
+    .innerHTML = numero;
+
+    if(numero >= 5000){
+
+        clearInterval(contador);
+
     }
 
 }, 30);
